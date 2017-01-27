b325aa9e4c057c56482ac37442fd4c7d1b3a0220
jaxkodex <jorge12745@gmail.com>
2017-01-26 23:48:15 -0500

    target1.7 java

--- auth-server/src/main/java/pe/org/yian/oauth/auth/server/OAuthAuthenticationServer.java
+++ auth-server/src/main/java/pe/org/yian/oauth/auth/server/OAuthAuthenticationServer.java
@@ -2,13 +2,19 @@ package pe.org.yian.oauth.auth.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import pe.org.yian.oauth.auth.server.config.OAuth2AuthorizationServerConfig;
 
 @SpringBootApplication
-public class OAuthAuthenticationServer {
+public class OAuthAuthenticationServer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OAuthAuthenticationServer.class);
+	}
 
 	public static void main (String[] args) {
 		SpringApplication.run(OAuthAuthenticationServer.class, args);
