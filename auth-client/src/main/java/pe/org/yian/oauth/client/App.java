e526345a2000a1c81adaf1d0dca1763d54bf0fb4
jaxkodex <jorge12745@gmail.com>
2017-05-28 22:22:36 -0500

    fix wrong reference

--- auth-client/src/main/java/pe/org/yian/oauth/client/App.java
+++ auth-client/src/main/java/pe/org/yian/oauth/client/App.java
@@ -1,19 +1,6 @@
 package pe.org.yian.oauth.client;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
-import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.AccessTokenRequest;
-import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import pe.org.yian.oauth.client.service.MeService;
 
 /**
  * Hello world!
