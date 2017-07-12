6b02303d963722c1575585f11cb4e844486011d5
jaxkodex <jorge12745@gmail.com>
2017-07-11 23:07:32 -0500

    version 2.0.3.release

--- auth-server/src/main/java/pe/org/yian/oauth/auth/server/config/WebSecurityConfig.java
+++ auth-server/src/main/java/pe/org/yian/oauth/auth/server/config/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +20,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/resources/**").permitAll();
-
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		http.requestMatchers().antMatchers("/uaa/**", "/oauth/authorize").and().authorizeRequests()
 		.antMatchers("/uaa/**").authenticated()
